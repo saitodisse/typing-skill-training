3f030959ed505f175a54117908aff11ad7efef87
Julio Saito <saitodisse@gmail.com>
2013-10-16 16:23:25 -0300

    refactored, but still not work like I want, I'll have to make tests

--- js/jquery.konami.js
+++ js/jquery.konami.js
@@ -1,41 +1,47 @@
 ;(function($, target){
+  "use strict";
+
   $.fn.konami = function(){
     var SEQUENCE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
     var newSeq = [];
     var expectedSeq = [];
     var actualIndice = 0;
 
-    var initializeNewSeq = function initializeNewSeq(){
+    var initializeNewSeq = function(){
       expectedSeq = [];
       for (var i = 0; i <= SEQUENCE.length; i++) {
         expectedSeq.push(SEQUENCE[i]);
-      };
-    }
+      }
+    };
 
-    $(target).on("keyup", function(event){
-      newSeq.push(event.which);
-
+    var checkKonamiCode = function(key){
+      newSeq.push(key);
+      var sameArrayUntiNow = SEQUENCE[actualIndice] === newSeq[actualIndice];
       var lastSEQindex = SEQUENCE.length - 1;
+
+      if(!sameArrayUntiNow){
+        console.log("wrong", newSeq);
+        newSeq = [];
+        newSeq.push(key);
+        actualIndice = 0;
+      }
+
       if(actualIndice === lastSEQindex){
         console.log("you did it!", newSeq);
         newSeq = [];
         actualIndice = 0;
       }
-
-      if(SEQUENCE[actualIndice] !== newSeq[actualIndice]){
-        console.log("wrong", newSeq);
-        newSeq = [];
-        actualIndice = 0;
-      }
-      else {
-        if(actualIndice > 0){
-          console.log("keep going...", newSeq);
-        }
+      else{
+        console.log("keep going...", newSeq);
         actualIndice++;
       }
 
+    };
+
+    $(target).on("keyup", function(event){
+      checkKonamiCode(event.which);
     });
 
     return this;
-  }
+  };
 })(jQuery, window);
