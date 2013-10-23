e2950ffd5a8987f415cd675cf8d69ba35f066706
Julio Saito <saitodisse@gmail.com>
2013-10-23 16:20:07 -0200

    sandbox.html

--- javascripts/jquery.konami.js
+++ javascripts/jquery.konami.js
@@ -1,79 +1,83 @@
 ;(function($){
   "use strict";
-  var eventTarget,
-      SEQUENCE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
-      tid,
-      index = 0,
-      start;
+  var me,
+      eventTarget;
 
   $.fn.konami = function(){
     eventTarget = this;
-    return this.data("konami", createKonami(this).init());
+    me = createKonami(this).init();
+    return this.data("konami", me);
   };
   $.fn.konami.off = function(){
-    clearInterval(tid);
-    eventTarget.trigger("konami.disabled");
-    eventTarget.off("keyup", keyUpKonami);
+    me.disable();
   };
 
-  function createKonami(container){
-    var me = {
-      container: container,
+  function createKonami(){
+    var  SEQUENCE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
+         tid,
+         index = 0,
+         start;
 
-      init: function(){
-        enablePlugin();
-        return this;
-      }
+    // private methods
+    var keyup = function(event){
+      eventTarget.trigger("konami.keyup", {which : event.which});
+      checkKonamiCode(event.which);
     };
 
-    return me;
-  }
+    var enable = function(){
+        eventTarget.trigger("konami.enabled");
+        eventTarget.on("keyup", keyup);
+    };
 
-  var enablePlugin = function(){
-    eventTarget.trigger("konami.enabled");
-    eventTarget.on("keyup", keyUpKonami);
-  };
-  var keyUpKonami = function(event){
-    checkKonamiCode(event.which);
-  };
+    var failKonami = function(){
+      index = 0;
+      clearInterval(tid);
+      eventTarget.trigger("konami.fail");
+    };
 
-  var failKonami = function(){
-    index = 0;
-    clearInterval(tid);
-    eventTarget.trigger("konami.fail");
-  };
+    var checkKonamiCode = function(key){
+      if(index === 0){
+        start = new Date();
+      }
 
-  var checkKonamiCode = function(key){
-    if(index === 0){
-      start = new Date();
-    }
+      clearInterval(tid);
+      var isCorret = SEQUENCE[index] === key;
 
-    clearInterval(tid);
-    var isCorret = SEQUENCE[index] === key;
+      if(isCorret){
+        index++;
+        tid = setTimeout(failKonami, 1000);
+      }
+      else{
+        return failKonami();
+      }
 
-    if(isCorret){
-      index++;
-      tid = setTimeout(failKonami, 1000);
-    }
-    else{
-      return failKonami();
-    }
+      if(index === SEQUENCE.length){
+        return function(){
+          index = 0;
+          clearInterval(tid);
+          eventTarget.trigger("konami.success", {executedTime : new Date() - start });
+        }();
+      }
 
-    if(index === SEQUENCE.length){
-      return function(){
-        index = 0;
+      eventTarget.trigger("konami.progress",
+        { expected: SEQUENCE[index-1],
+          received: key,
+          index: index
+        });
+    };
+
+    return {
+      // public methods
+      init: function(){
+        enable();
+        return this;
+      },
+      disable: function(){
         clearInterval(tid);
-        eventTarget.trigger("konami.success", {executedTime : new Date() - start });
-      }();
-    }
-
-    eventTarget.trigger("konami.progress",
-      { expected: SEQUENCE[index-1],
-        received: key,
-        index: index
-      });
-  };
-
-
+        eventTarget.trigger("konami.disabled");
+        eventTarget.off("keyup", me.keyup);
+      }
+    };
+  }
 
 })(jQuery, window);
--- javascripts/specs/jquery.konami.spec.js
+++ javascripts/specs/jquery.konami.spec.js
@@ -1,9 +1,50 @@
+var globalObj = this;
 describe("konami keyup tests", function() {
-  it("function $.fn.konami exists", function() {
+  var konami;
+
+  beforeEach(function() {
+    var konami = $(globalObj).konami();
+  });
+
+  it("$.fn.konami is defined", function() {
     expect($.prototype.konami).toBeDefined();
     expect($.fn.konami).toBeDefined();
   });
-  it("when initiated konami.enabled was triggered", function() {
-    //expect($.fn.konami).toBeDefined();
+
+  it("konami.enabled triggered on init", function() {
+    var spyEvent = spyOnEvent(globalObj, 'konami.enabled');
+    konami = $(globalObj).konami();
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+
+  it("konami.enabled triggered on init on other element", function() {
+    var otherElement = $("<div/>");
+    var spyEvent = spyOnEvent(otherElement, 'konami.enabled');
+    konami = $(otherElement).konami();
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+
+  it("konami.off() trigger konami.disabled ", function() {
+    var spyEvent = spyOnEvent(globalObj, 'konami.disabled');
+    $(globalObj).konami.off();
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+
+  it("konami.keyup triggered on keyup", function() {
+    var spyEvent = spyOnEvent(globalObj, 'konami.keyup');
+    $(globalObj).trigger($.Event("keyup", { which: 64 }));
+    expect(spyEvent).toHaveBeenTriggered();
+  });
+
+  it("konami.keyup gives the event.which", function() {
+    var keyPressed;
+    $(globalObj).on("konami.keyup", function(ev, opt){
+      keyPressed = opt.which;
+    });
+
+    $(globalObj).trigger($.Event("keyup", { which: 64 }));
+
+    expect(keyPressed).toEqual(64);
   });
 });
+
